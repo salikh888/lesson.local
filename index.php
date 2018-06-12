1611fb2c183989050c7c1be820380b73c408904b
Salikh Sayfutdinov <salikh.sayfutdinov@gmail.com>
2018-06-12 16:44:28 +0500

    урок 7,2

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 12.06.2018
+ * Time: 15:03
+ */
+
+
+//$colors = ['green' => '#090', 'blue' => '#009', 'red' => '#900'];
+//$bgcolor = '#fff'; // Цвет фона по умолчанию (белый)
+//
+////--Проверяем, есть ли уже сохраненный в куки bgcolor (также проверяем на корректность, т.к. куки легко подменить)
+//if ( isset($_COOKIE['bgcolor']) && in_array($_COOKIE['bgcolor'], $colors) )
+//{
+//    //--Если есть, используем сохраненное значение
+//    $bgcolor = $_COOKIE['bgcolor'];
+//}
+//
+////--Проверяем, был ли передан GET-параметр bgcolor и имеется ли он в нашем массиве. А то, знаете ли, могут передать мало ли чего, а нам потом расхлебывать ;)
+//if ( isset($_GET['bgcolor']) && in_array($_GET['bgcolor'], $colors) )
+//{
+//    $bgcolor = $_GET['bgcolor']; //--Это будет наш новый фон для страницы
+//    setcookie('bgcolor', $bgcolor); //--Сохраняем значение цвета фона в куки
+//}
+
+
+$bgcolor = isset($_COOKIE['bgcolor']) ? $_COOKIE['bgcolor'] : 'red';
+if (isset($_POST['submit']) && in_array($_POST['bgcolor'], ['red', 'green', 'blue'])) {
+    $bgcolor = $_POST['bgcolor'];
+    setcookie('bgcolor', $bgcolor);
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style type="text/css">
+        body{
+            background-color: <?= "$bgcolor";?>;
+            /*background-color: red;*/
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+<form class="form form-row p-md-5" action="index.php" method="post">
+    <div class="container bgc">
+        <div class="">
+            <input class="radio" type="radio" id="check1" name="bgcolor" value="red"
+            <?php if ($bgcolor === "red"): ?>checked="checked"<?php endif; ?>>
+            <label for="check1">Красный</label>
+        </div>
+        <div class="">
+            <input class="radio" type="radio" id="check2" name="bgcolor" value="green"
+            <?php if ($bgcolor === "green"): ?>checked="checked"<?php endif; ?>>
+            <label for="check2">Зелёный</label>
+        </div>
+        <div class="">
+            <input class="radio" type="radio" id="check3" name="bgcolor" value="blue"
+            <?php if ($bgcolor === "blue"): ?>checked="checked"<?php endif; ?>>
+            <label for="check3">Синий</label>
+        </div>
+        <input class="submit btn btn-primary hvr-grow" type="submit" name="submit" value="Перекрасить">
+    </div>
+    </div>
+</form>
+</body>
+</html>
+
--- lesson-7-1.php
+++ lesson-7-1.php
@@ -24,21 +24,21 @@ if (isset($_POST['submit']) && empty($_POST['sayt'])) {
     <title>Document</title>
 </head>
 <body>
-<form class="form form-row p-5" action="lesson-7-1.php" method="post">
+<form class="form form-row p-md-5" action="lesson-7-1.php" method="post">
     <div class="container bgc">
-        <div class="text-center">
+        <div class="">
             <input class="radio" type="radio" id="check1" name="sayt" value="Google">
             <label for="check1">Google</label>
         </div>
-        <div class="text-center">
+        <div class="">
             <input class="radio" type="radio" id="check2" name="sayt" value="VK">
             <label for="check2">VK</label>
         </div>
-        <div class="text-center">
+        <div class="">
             <input class="radio" type="radio" id="check3" name="sayt" value="myrusakov">
             <label for="check3">myrusakov</label>
         </div>
-        <input class="submit btn btn-primary hvr-grow " type="submit" name="submit" value="Перейти">
+        <input class="submit btn btn-primary hvr-grow" type="submit" name="submit" value="Перейти">
     </div>
     </div>
 </form>
