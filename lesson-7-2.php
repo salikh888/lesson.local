e6e1090a83163b015ca42da09aa323b869cffdaa
Salikh Sayfutdinov <salikh.sayfutdinov@gmail.com>
2018-06-13 12:13:59 +0500

    урок 7,2 пройден

--- lesson-7-2.php
+++ lesson-7-2.php
@@ -25,8 +25,15 @@
 //}
 
 
-$bgcolor = isset($_COOKIE['bgcolor']) ? $_COOKIE['bgcolor'] : 'red';
-if (isset($_POST['submit']) && in_array($_POST['bgcolor'], ['red', 'green', 'blue'])) {
+//$bgcolor = isset($_COOKIE['bgcolor']) ? $_COOKIE['bgcolor'] : '#fff';
+//if (isset($_POST['submit']) && in_array($_POST['bgcolor'], ['red', 'green', 'blue'])) {
+//    $bgcolor = $_POST['bgcolor'];
+//    setcookie('bgcolor', $bgcolor);
+//}
+if (isset($_COOKIE['bgcolor'])) {
+    $bgcolor = $_COOKIE['bgcolor'];
+} else $bgcolor = '#fff';
+if (isset($_POST['submit']) && !empty($_POST['bgcolor'])){
     $bgcolor = $_POST['bgcolor'];
     setcookie('bgcolor', $bgcolor);
 }
@@ -41,27 +48,26 @@ if (isset($_POST['submit']) && in_array($_POST['bgcolor'], ['red', 'green', 'blu
     <style type="text/css">
         body{
             background-color: <?= "$bgcolor";?>;
-            /*background-color: red;*/
         }
     </style>
     <title>Document</title>
 </head>
 <body>
-<form class="form form-row p-md-5" action="index.php" method="post">
+<form class="form form-row p-md-5" action="lesson-7-2.php" method="post">
     <div class="container bgc">
         <div class="">
             <input class="radio" type="radio" id="check1" name="bgcolor" value="red"
-            <?php if ($bgcolor === "red"): ?>checked="checked"<?php endif; ?>>
+<!--            --><?php //if ($bgcolor === "red"): ?><!--checked="checked"--><?php //endif; ?><!-->-->
             <label for="check1">Красный</label>
         </div>
         <div class="">
             <input class="radio" type="radio" id="check2" name="bgcolor" value="green"
-            <?php if ($bgcolor === "green"): ?>checked="checked"<?php endif; ?>>
+<!--            --><?php //if ($bgcolor === "green"): ?><!--checked="checked"--><?php //endif; ?><!-->-->
             <label for="check2">Зелёный</label>
         </div>
         <div class="">
             <input class="radio" type="radio" id="check3" name="bgcolor" value="blue"
-            <?php if ($bgcolor === "blue"): ?>checked="checked"<?php endif; ?>>
+<!--            --><?php //if ($bgcolor === "blue"): ?><!--checked="checked"--><?php //endif; ?><!-->-->
             <label for="check3">Синий</label>
         </div>
         <input class="submit btn btn-primary hvr-grow" type="submit" name="submit" value="Перекрасить">
