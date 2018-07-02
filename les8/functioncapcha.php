62d2605337b46e9e891c643f5b014d103bb7d7be
Salikh Sayfutdinov <salikh.sayfutdinov@gmail.com>
2018-07-02 19:09:32 +0500

    Добавил проверку на существования массив пост

--- les8/functioncapcha.php
+++ les8/functioncapcha.php
@@ -16,8 +16,8 @@ function randnumorlet()
     return $text;
 }
 
-$widht = 400;
-$heiqht = 150;
+$widht = 200;
+$heiqht = 75;
 
 $im = '';
 $x = '';
@@ -34,55 +34,72 @@ for ($i = 1; $i <= 4; $i++) {
 
     switch ($i) {
         case $i == 1;
-            $x = rand(10, 90);
+            $x = rand(10, 48);
             break;
         case $i == 2;
-            $x = rand(110, 200);
+            $x = rand(50, 98);
             break;
         case $i == 3;
-            $x = rand(210, 300);
+            $x = rand(100, 158);
             break;
         case $i == 4;
-            $x = rand(310, 390);
+            $x = rand(160, 188);
             break;
 
     }
+    imageSetThickness($im, 2);
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageSetPixel($im, rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
 
-    imageSetPixel($im, rand(10, 390), rand(10, 140), imageColorAllocate($im, 8, 24, 89));
-    imageTtfText($im, 30, 0, $x, rand(30, 100), imageColorAllocate($im, 8, 24, 89), 'fonts/times.ttf', $text);
+    imageline($im, rand(5, 195), rand(60, 20), rand(5, 195), rand(60, 20), imageColorAllocate($im, 8, 24, 89));
+    imageline($im, rand(5, 195), rand(60, 20), rand(5, 195), rand(60, 20), imageColorAllocate($im, 240, 240, 240));
+    imageTtfText($im, 30, 0, $x, rand(60, 20), imageColorAllocate($im, 8, 24, 89), 'fonts/times.ttf', $text);
     $textarr[] = $text;
 
 }
 
-if (empty($_SESSION['textarr'])) {
+if (isset($_SESSION['textarr'])) {
     $_SESSION['textarr'] = $textarr;
 }
 
+$kapcha = false;
 ob_start();
 imagePng($im);
 $image = ob_get_contents();
 ob_end_clean();
+print_r($_POST['submit']);
+
 if (isset($_POST['submit']) && empty($_POST['kapcha'])) {
     unset($_SESSION['textarr']);
 }
+
 if (isset($_POST['submit'])) {
     $kapchaArr = $_POST['kapcha'];
     $kapchaArr = str_split($kapchaArr);
-    print_r($kapchaArr);
-    echo '<br>';
-    print_r($_SESSION['textarr']);
     if ($kapchaArr == $_SESSION['textarr']) {
         $kapcha = true;
+    } elseif ($kapchaArr != $_SESSION['textarr']) {
+        unset($_SESSION['textarr']);
     }
 
 }
-
+//print_r($kapchaArr);
+//echo '<br>';
+print_r($_SESSION['textarr']);
 ?>
-
 <?php if ($kapcha) { ?>
     <?php unset($_SESSION['textarr']); ?>
-    <p>Вы не робот</p>
-    <a href='functioncapcha.php?f=logout'>Выход</a>
 <?php } else  { ?>
 <!doctype html>
 <html lang="en">
@@ -90,14 +107,44 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="/bootstrap-4.1.1/dist/css/bootstrap.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-<form method="post" action="functioncapcha.php">
-    <?= '<img src="data:image/png;base64,' . base64_encode($image) . '" />' ?>
-    <input type="text" name="kapcha">
-    <input type="submit" name="submit">
-    <?php } ?>
+
+<div class="container h-100">
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <form class="form-control-range row align-items-center d-flex" style="height: 600px" method="post"
+                  action="functioncapcha.php">
+
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <?= '<img src="data:image/png;base64,' . base64_encode($image) . '" />' ?>
+                    </div>
+
+                    <div class="col-12 p-2 d-flex justify-content-center">
+                        <div class="form-group form-inline">
+                            <input class="form-control" style="width: 100px" maxlength="4" type="text" name="kapcha">
+                            <button class="btn btn-secondary" name="submit">Отправить</button>
+                            <?php } ?>
+                        </div>
+                    </div>
+                    <div class="col-12 d-flex justify-content-center">
+                        <div class="form-group form-inline">
+                        <?php if ($kapcha) { ?>
+                            <p>Вы не робот</p>
+                            <a href='functioncapcha.php?f=logout'>Назад</a>
+                        <?php } ?>
+                        </div>
+                </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
